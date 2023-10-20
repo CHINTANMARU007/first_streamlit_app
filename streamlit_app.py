8b99061b9137316ab463975fd90f45af03e23fda
CHINTANMARU007 <148560322+CHINTANMARU007@users.noreply.github.com>
2023-10-20 19:45:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
 import streamlit
 streamlit.title('My partents New Healthy dinner')
 
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, spinace & Rocket Smoothie')
+streamlit.text('Hard-Boiled free- range Egg')
+
+
+
+
